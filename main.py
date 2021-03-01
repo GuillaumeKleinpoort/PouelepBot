64bb4da74b6d4731188f9e75ece3ccc67b44d97f
Guillaume Kleinpoort <guillaume.kleinpoort@gmail.com>
2021-03-01 21:33:10 +0000

    pouelep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import discord
+import os
+
 from keep_alive import keep_alive
+
 
 client = discord.Client()
 
@@ -7,9 +10,13 @@ client = discord.Client()
 async def on_ready():
     print('Pouelep ? {}'.format(client))
     
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if str.lowercase(message.content) == 'pouelep':
+    if str.lower(message.content) == 'pouelep':
       await message.channel.send('POUELEP')
+
+keep_alive()
+client.run("ODE2MDQ5OTkxNTg3NTk0MjQx.YD1Txw.hylWBAc1mlF40fa755ZNcnx1vUc")
