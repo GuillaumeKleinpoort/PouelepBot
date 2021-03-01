b2a783e4a58372b9df0a95b155907e24dc7620bb
Guillaume Kleinpoort <guillaume.kleinpoort@gmail.com>
2021-03-01 22:15:32 +0000

    nerfing vocal poueleping

--- main.py
+++ main.py
@@ -19,14 +19,15 @@ async def on_message(message):
       await message.channel.send('POUELEP')
 
 
-    voice_state = message.author.voice
-
-    if voice_state is not None:
-        channel = message.author.voice.channel
-        vc = await channel.connect()
-        vc.play(discord.FFmpegPCMAudio("pouelep.mp3"))
-        time.sleep(1.5)
-        await vc.disconnect()
-
+    try:
+      voice_state = message.author.voice
+      if voice_state is not None :
+          channel = message.author.voice.channel
+          vc = await channel.connect()
+          vc.play(discord.FFmpegPCMAudio("pouelep.mp3"))
+          time.sleep(1.5)
+          await vc.disconnect()
+    except discord.errors.ClientException:
+      pass
 keep_alive()
 client.run("")
